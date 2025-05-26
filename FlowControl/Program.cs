56d879c8a98417a1f7b4d9ba61fa803b4f30a098
Diana Todorova <diana.n.todorova@gmail.com>
2025-05-26 10:28:06 +0200

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FlowControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isStarted = true;
+            while (isStarted)
+            {
+                Console.WriteLine("---Main Menu---");
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1/ Exit the program");
+                Console.WriteLine("2/ Buy a tycket.");
+                Console.Write("Your choise is: ");
+                var choise =Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "0":
+                    isStarted = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+        }
+    }
+}
